796a303dc7ad9fb8502d19d00410dd4ffe5a78e0
Winlypse <nts3@o2.pl>
2024-10-22 23:47:53 +0200

    Changes!

--- src/main/kotlin/winlyps/noInfiniteWaterSource/NoInfiniteWaterSource.kt
+++ src/main/kotlin/winlyps/noInfiniteWaterSource/NoInfiniteWaterSource.kt
@@ -6,9 +6,14 @@ class NoInfiniteWaterSource : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("waterSourceConversion", "false")
+        }
+        logger.info("NoInfiniteWaterSource plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoInfiniteWaterSource plugin has been disabled.")
     }
 }
